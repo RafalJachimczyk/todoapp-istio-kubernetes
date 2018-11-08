da47e0cfe446a50badf24746dd5fa09c94822353
Rafal Jachimczyk <Rafal.Jachimczyk@bbc.co.uk>
2018-11-08 14:29:32 +0000

    chore: updates build script

--- src/todo-api/build_service.sh
+++ src/todo-api/build_service.sh
@@ -2,5 +2,5 @@ set -o errexit
 
 SCRIPTDIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
 
-docker build -t todoapp-api --build-arg service_version=v1 "${SCRIPTDIR}"
-docker build -t todoapp-api --build-arg service_version=v2 "${SCRIPTDIR}"
+docker build -t gcr.io/todoapp-221909/todoapp-api:v1 --build-arg service_version=v1 "${SCRIPTDIR}"
+docker build -t gcr.io/todoapp-221909/todoapp-api:v2 --build-arg service_version=v2 "${SCRIPTDIR}"
