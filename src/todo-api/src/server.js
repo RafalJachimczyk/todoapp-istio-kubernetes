b3c2eacf84f4a95fe79b4b4658025ef79cd552da
Rafal Jachimczyk <Rafal.Jachimczyk@bbc.co.uk>
2018-12-13 16:11:24 +0000

    feat: add CORS header, fix JSON+api data format

--- src/todo-api/src/server.js
+++ src/todo-api/src/server.js
@@ -3,33 +3,36 @@ const express = require('express');
 
 const app = express();
 
-app.use('/todos', (req, res) => {
+app.use(function (req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
+});
 
+app.use('/todos', (req, res) => {
+    console.log('TODO-API HTTP Get /todos');
     const todos = {
         'data': [
             {
-                'attributes': [
-                    {
-                        'name': 'Fix the car',
-                        'done': false
-                    }
-                ]
+                'attributes':
+                {
+                    'name': 'Fix the car',
+                    'done': false
+                }
             },
             {
-                'attributes': [
-                    {
-                        'name': 'Feed the cat',
-                        'done': false
-                    }
-                ]
+                'attributes':
+                {
+                    'name': 'Feed the cat',
+                    'done': true
+                }
             },
             {
-                'attributes': [
-                    {
-                        'name': 'Eat a doughnut',
-                        'done': false
-                    }
-                ]
+                'attributes':
+                {
+                    'name': 'Eat a doughnut',
+                    'done': false
+                }
             },
         ]
     }
